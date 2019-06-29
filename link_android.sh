8f8b747818e805dde1604708f0d02f4bcb1c61c4
John Wilkinson <me@jcwilk.com>
2019-06-28 18:20:47 -0700

    Further customizations and script tweaking
    Added a README yay

--- link_android.sh
+++ link_android.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-find ./android -type d -mindepth 1 -maxdepth 1 -printf '%P\n' | while read file
+find ./android -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | while read file
 do
-  echo "deleting and linking $file"
-  rm -rf "/root/$file" #what's the worst that could happen! <3
-  ln -s "$PWD/android/$file" /root/
+  echo "deleting and linking $file" &&
+  rm -rf "/root/$file" && #what's the worst that could happen! <3
+  ln -s "$PWD/android/$file" /root/ ||
+  echo "failed!" && exit 1 
 done
+
+#link in any jdks contained in this directory, if any
+find . -mindepth 1 -maxdepth 1 -type d -iname 'jdk*' -printf '%P\n' | while read file
+do
+  echo "deleting and linking $file" &&
+  rm -rf "/usr/lib/jvm/$file" && #nothing bad here either, I'm sure ;) :*
+  ln -s "$PWD/$file" /usr/lib/jvm/ &&
+  rm -f "/usr/lib/jvm/default-java" &&
+  ln -s "/usr/lib/jvm/$file" /usr/lib/jvm/default-java ||
+  echo "failed!" && exit 1
+done
+
+echo "linked android and jdk successfully"
---
+++ store_public.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#TODO - this screws up arguments with spaces u_u
+
+if [[ $# -eq 1 ]]
+then
+  FULL_PATH=$(readlink -f $1)
+  if [[ -e $FULL_PATH ]]
+  then
+    echo "copying $FULL_PATH"
+    cp -r --parents --preserve=all "$FULL_PATH" ./fs/
+  else
+    >&2 echo "path not found! $FULL_PATH"
+    exit 1
+  fi
+else
+  >&2 echo "ERROR - wrong number of arguments passed to store_public"
+  exit 1
+fi
+
+
